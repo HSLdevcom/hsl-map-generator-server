582b9b084084605ffab45463b87282e27730a355
Sebastian <sebastian.hognabba@gmail.com>
2016-09-16 16:06:30 +0300

    added endpoints for getting routes and stops

--- src/hsl-map-generator-server.js
+++ src/hsl-map-generator-server.js
@@ -1,23 +1,73 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-const router = require('koa-router')();
-const bodyParser = require('koa-bodyparser')();
-const imageGenerator = require('./imageGenerator');
-// const stopLabelGenerator = require('./stopLabelGenerator');
+const router = require("koa-router")();
+const bodyParser = require("koa-bodyparser")();
+const fs = require('file-system');
+const path = require("path");
+const dataPath = path.join(__dirname, "..", "data");
 
-router.post('/generateImage', ctx =>
-  new Promise((resolve) =>
+const imageGenerator = require("./imageGenerator");
+// const stopLabelGenerator = require('./stopLabelGenerator');
+const Routes = require("../data/routes.json");
+const Shapes = JSON.parse(fs.readFileSync(`${dataPath}/shapes.geojson`, "utf8"));
+const Stops = JSON.parse(fs.readFileSync(`${dataPath}/stops.geojson`, "utf8"));
+const PORT = 8000;
+
+
+router.post("/generateImage", ctx =>
+    new Promise(resolve =>
     imageGenerator(
-      ({ data }) => {
-        ctx.status = 200; // eslint-disable-line no-param-reassign
-        ctx.type = 'image/png'; // eslint-disable-line no-param-reassign
-        ctx.body = data; // eslint-disable-line no-param-reassign
-        resolve();
-      },
-      ctx.request.body
+        (data) => {
+            ctx.status = 200; // eslint-disable-line no-param-reassign
+            ctx.type = "image/png"; // eslint-disable-line no-param-reassign
+            ctx.body = data; // eslint-disable-line no-param-reassign
+            resolve();
+        },
+        ctx.request.body
     )
   )
 );
+
+router.get("/routes", ctx =>
+    new Promise((resolve) => {
+        ctx.response.set("Access-Control-Allow-Origin", "*");
+        ctx.status = 200; // eslint-disable-line no-param-reassign
+        ctx.body = Routes; // eslint-disable-line no-param-reassign
+        resolve();
+    })
+);
+
+router.get("/routeInfo/:id", ctx =>
+    new Promise((resolve) => {
+        ctx.response.set("Access-Control-Allow-Origin", "*");
+        ctx.status = 200; // eslint-disable-line no-param-reassign
+        ctx.body = Shapes.features.filter(feature => // eslint-disable-line no-param-reassign
+            feature.properties.shape_id.startsWith(ctx.params.id)
+        );
+        resolve();
+    })
+);
+
+router.get("/stopInfo/:routeId", ctx =>
+    new Promise((resolve) => {
+        ctx.response.set("Access-Control-Allow-Origin", "*");
+        ctx.status = 200; // eslint-disable-line no-param-reassign
+        ctx.body = Stops.features.filter(feature => // eslint-disable-line no-param-reassign
+            feature.properties.route.startsWith(ctx.params.routeId)
+        );
+        resolve();
+    })
+);
+
+router.get("/stops", ctx =>
+    new Promise((resolve) => {
+        ctx.response.set("Access-Control-Allow-Origin", "*"); // eslint-disable-line no-param-reassign
+        ctx.status = 200; // eslint-disable-line no-param-reassign
+        ctx.body = Stops; // eslint-disable-line no-param-reassign
+        resolve();
+    })
+);
+
 
 // router.post('/generateStopLabels', ctx =>
 //   new Promise((resolve) =>
@@ -34,7 +84,12 @@ router.post('/generateImage', ctx =>
 // );
 
 app
-  .use(bodyParser)
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .listen(4000);
+    .use(bodyParser)
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(PORT, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(`Listening at port ${PORT}`);
+    });
